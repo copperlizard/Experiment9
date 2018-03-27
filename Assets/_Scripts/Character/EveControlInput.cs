ac3bab44a75638d4de091ebd4103f280eaa04061
Cam McKasson <cam@mckassons.com>
2018-03-27 12:48:28 -0700

    I did some stuff...

---
+++ Assets/_Scripts/Character/EveControlInput.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(EveController))]
+public class EveControlInput : MonoBehaviour {
+
+    private EveController m_eveController;
+
+    private Vector2 m_move = Vector2.zero;
+
+	// Use this for initialization
+	void Start ()
+    {
+        m_eveController = GetComponent<EveController>();
+        if (m_eveController == null)
+        {
+            Debug.Log("m_eveController not found!");
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        GetInput();
+    }
+
+    private void GetInput()
+    {
+        //Add control options here
+
+        GetXBOXcontrolInput();
+
+        m_eveController.Move(m_move);
+    }
+
+    private void GetXBOXcontrolInput()
+    {
+        m_move.x = Input.GetAxis("Xaxis");
+        m_move.y = Input.GetAxis("Yaxis");
+    }
+}
---
+++ Assets/_Scripts/Character/EveController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class EveController : MonoBehaviour
+{
+    [SerializeField]
+    private float m_groundCheckDist = 0.01f, m_lerpToGround = 0.1f;
+
+    private Rigidbody m_rigidBody;
+
+    private RaycastHit m_groundAt;
+
+    private bool m_grounded = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_rigidBody = GetComponent<Rigidbody>();
+        if (m_rigidBody == null)
+        {
+            Debug.Log("m_rigidBody not found!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        checkGround();
+    }
+
+    public void checkGround()
+    {
+        if(!Physics.Raycast(transform.position + Vector3.up * 0.25f, Vector3.down, out m_groundAt, m_groundCheckDist + 0.25f, LayerMask.NameToLayer("Default")))
+        {
+            m_grounded = true;
+            m_rigidBody.useGravity = false;
+            transform.position = new Vector3(transform.position.x, Mathf.Lerp(transform.position.y, m_groundAt.point.y, m_lerpToGround), transform.position.z);
+        }
+        else
+        {
+            m_grounded = false;
+            m_rigidBody.useGravity = true;
+        }
+    }
+
+    public void Move(Vector2 input)
+    {
+        m_rigidBody.AddForce(new Vector3(input.x, 0.0f, input.y));
+    }
+}
