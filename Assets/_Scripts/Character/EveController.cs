b8a3c805ebd85a2a2d3794b45bdbcfae538bef6c
Cam McKasson <cam@mckassons.com>
2018-03-28 13:10:11 -0700

    started working on eve's animator; need to sort out quick-turn bug...

--- Assets/_Scripts/Character/EveController.cs
+++ Assets/_Scripts/Character/EveController.cs
@@ -6,28 +6,61 @@ using UnityEngine;
 public class EveController : MonoBehaviour
 {
     [SerializeField]
-    private float m_groundCheckDist = 0.01f, m_lerpToGround = 0.1f;
+    private float m_groundCheckDist = 0.01f, m_lerpToGround = 0.1f, m_StationaryTurnSpeed = 180.0f, m_MovingTurnSpeed = 360.0f, m_RunCycleLegOffset = 0.0f;
+
+    private Animator m_animator;
+
+    private Camera m_camera;
+
+    private CapsuleCollider m_capCollider;
 
     private Rigidbody m_rigidBody;
 
     private RaycastHit m_groundAt;
 
-    private bool m_grounded = true;
+    private Vector3 m_move = Vector3.zero;
+
+    private float m_forward = 0.0f, m_turn = 0.0f, m_jumpLeg = -1.0f;
+
+    private bool m_grounded = true, m_jumping = false, m_crouching = false;
 
     // Use this for initialization
     void Start()
     {
+        m_animator = GetComponent<Animator>();
+        if (m_animator == null)
+        {
+            Debug.Log("m_animator not found!");
+        }
+
         m_rigidBody = GetComponent<Rigidbody>();
         if (m_rigidBody == null)
         {
             Debug.Log("m_rigidBody not found!");
+        }
+        else
+        {
+            m_rigidBody.freezeRotation = true;
+        }
+
+        //m_camera = GetComponent<Camera>();
+        m_camera = Camera.main;
+        if (m_camera == null)
+        {
+            Debug.Log("m_camera not found!");
+        }
+
+        m_capCollider = GetComponent<CapsuleCollider>();
+        if (m_capCollider == null)
+        {
+            Debug.Log("m_capCollider not found!");
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        UpdateAnimator();
     }
 
     private void FixedUpdate()
@@ -35,7 +68,24 @@ public class EveController : MonoBehaviour
         checkGround();
     }
 
-    public void checkGround()
+    private void UpdateAnimator()
+    {
+        m_animator.SetFloat("Forward", m_forward);
+        m_animator.SetFloat("Turn", m_turn);
+
+        if (m_grounded && m_forward > 0.0f)
+        {
+            float runCycle = Mathf.Repeat(m_animator.GetCurrentAnimatorStateInfo(0).normalizedTime + m_RunCycleLegOffset, 1);
+            m_jumpLeg = (runCycle < 0.5f ? 1 : -1);
+        }
+        m_animator.SetFloat("JumpLeg", m_jumpLeg);
+
+        m_animator.SetBool("Grounded", m_grounded);
+        m_animator.SetBool("Jumping", m_jumping);
+        m_animator.SetBool("Crouching", m_crouching);
+    }
+
+    private void checkGround()
     {
         if(!Physics.Raycast(transform.position + Vector3.up * 0.25f, Vector3.down, out m_groundAt, m_groundCheckDist + 0.25f, LayerMask.NameToLayer("Default")))
         {
@@ -50,8 +100,61 @@ public class EveController : MonoBehaviour
         }
     }
 
+    private void RotateCharacter()
+    {
+        // In addition to root rotation in the animation
+        float turnSpeed = Mathf.Lerp(m_StationaryTurnSpeed, m_MovingTurnSpeed / Mathf.Max(1.0f, m_forward), m_forward);
+        transform.Rotate(0, m_turn * turnSpeed * Time.deltaTime, 0);
+    }
+
+    private void QuickTurn()
+    {       
+        Quaternion tarRot = Quaternion.LookRotation(transform.TransformDirection(m_move), Vector3.up);
+        StartCoroutine(QuickTurning(transform.rotation, tarRot));
+    }
+
+    private IEnumerator QuickTurning(Quaternion startRot, Quaternion tarRot)
+    {
+        //m_headLook = false;
+
+        AnimatorStateInfo animState = m_animator.GetCurrentAnimatorStateInfo(0);
+
+        float ang = Quaternion.Angle(startRot, tarRot);
+        if (m_jumpLeg > 0.0f)
+        {
+            ang = ang - 360.0f;
+        }
+
+        while (animState.normalizedTime < 1.0f && (animState.IsName("Eve180LeftFoot1") || animState.IsName("Eve180RightFoot1")))
+        {
+            transform.rotation = startRot * Quaternion.Euler(0.0f, -ang * Mathf.SmoothStep(0.0f, 1.0f, animState.normalizedTime), 0.0f);
+
+            animState = m_animator.GetCurrentAnimatorStateInfo(0);
+            yield return null;
+        }
+
+        transform.rotation = tarRot;
+        
+        yield return null;
+    }
+
     public void Move(Vector2 input)
     {
-        m_rigidBody.AddForce(new Vector3(input.x, 0.0f, input.y));
+        if (m_grounded)
+        {
+            Quaternion rot = Quaternion.Euler(0.0f, m_camera.transform.rotation.eulerAngles.y - transform.rotation.eulerAngles.y, 0.0f);
+            m_move = Vector3.Lerp(m_move, rot * new Vector3(input.x, 0.0f, input.y), 10.0f * Time.deltaTime);
+            m_forward = m_move.z;
+            m_turn = m_move.x;
+        }
+
+        if (m_forward >= 0.0f)
+        {
+            RotateCharacter();
+        }
+        else
+        {
+            QuickTurn(); //NOT WORKING!!!
+        }
     }
 }
